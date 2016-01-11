e86cc3f161c3a1325e1a6fc934412611136a94aa
Habib <flyingwithoutname@gmail.com>
2016-01-11 18:57:28 +0700

    heroku

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -13,15 +13,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -91,7 +82,7 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangoblog',
+        'NAME': 'blogsite',
         'USER': 'postgres',
         'PASSWORD': 'hardrock',
     }
@@ -135,7 +126,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
 
 STATIC_URL = '/static/' 
 
